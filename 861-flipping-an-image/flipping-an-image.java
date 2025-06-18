faa4591ba89c95d07ec6096f903ef5bf578860c3
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-06-18 20:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (77.52%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -1,32 +1,25 @@
 class Solution {
-    public int[][] flipAndInvertImage(int[][] image) {
-         for (int i = 0; i < image.length; i++) {
-            swap(image[i], 0, image[i].length - 1);
+    public int[][] flipAndInvertImage(int[][] arr) {
+         for (int i = 0; i < arr.length; i++) {
+            swap(arr[i]);
         }
-
-        for (int i = 0; i < image.length; i++) {
-            for (int j = 0; j < image[i].length; j++) {
-                if (image[i][j] == 1) {
-                    image[i][j] = 0;
-                } else {
-                    image[i][j] = 1;
+        for (int[] x : arr) {
+            for(int i=0;i<x.length;i++){
+                if(x[i]==0){
+                    x[i]=1;
+                }else{
+                    x[i]=0;
                 }
             }
         }
-
-        return image;
-    }
-     public int[] swap(int[] arr, int start, int end) {
-     
-        while (start < end) {
-            int temp = arr[end];
-            arr[end] = arr[start];
-            arr[start] = temp;
-            start++;
-            end--;
-        }
         return arr;
-
     }
-
+    static void swap(int[] arr) {
+        int mid = arr.length / 2;
+        for (int i = 0; i < mid; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+    }
 }
