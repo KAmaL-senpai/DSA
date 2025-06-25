1ce63639c9900c4ce159513ac3b299016ae25ed7
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-06-25 23:02:19 +0530

    Time: 1 ms (41.38%) | Memory: 42 MB (46.60%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; i++) {
+            if (Arrays.deepEquals(rotate90(mat), target)) {
+                return true;
+            }
+             mat = rotate90(mat);
+        }
+        return false;
+    }
+    
+    public int[][] rotate90(int[][] arr) {
+        int[][] ans = new int[arr.length][arr[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                ans[j][arr.length - 1 - i] = arr[i][j];
+            }
+        }
+
+        return ans;
+    }
+}
