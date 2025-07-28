c30fd2870a74b9ae3c92a6e68bf8502208b856ab
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-07-28 21:50:41 +0530

    Time: 7 ms (4.73%) | Memory: 45.9 MB (15.63%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,19 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        boolean firstRow = false;
+        boolean firstCol = false;
         int n = matrix.length;
         int m = matrix[0].length;
-        int col0 = 1;
+        
 
         // Marking the elements on the 0th row and col
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
+                    if (i == 0)
+                        firstRow = true;
+                    if (j == 0)
+                        firstCol = true;
+                    matrix[0][j] = 0;
                     matrix[i][0] = 0;
-                    if (j != 0) {
-                        matrix[0][j] = 0;
-                    } else {
-                        col0 = 0;
-                    }
                 }
             }
         }
@@ -30,14 +32,14 @@ class Solution {
         }
 
         // Edge case mark the top row first
-        if (matrix[0][0] == 0) {
+        if (firstRow) {
             for (int i = 0; i < m; i++) {
                 matrix[0][i] = 0;
             }
         }
 
         // Then do that first col
-        if (col0 == 0) {
+        if (firstCol) {
             for (int i = 0; i < n; i++) {
                 matrix[i][0] = 0;
             }
