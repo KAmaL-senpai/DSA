57b3ab283bff11a9a0c12b14e727423798f6caab
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-08-19 20:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (67.18%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,14 +1,38 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        boolean ans=false;
-        for(int i:nums){
-          if(i==target){
-               ans=true;
-             
-               break;
-          }
+          int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // Handle duplicates: shrink both ends
+            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                start++;
+                end--;
+                continue;
+            }
+            // Left half is sorted
+            else if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
         }
-return ans;
-       
+
+        return false;
     }
 }
