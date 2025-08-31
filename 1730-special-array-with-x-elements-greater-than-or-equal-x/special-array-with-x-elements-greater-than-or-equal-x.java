df7b4205b940d9ab4adbeef5f56c8a2219518817
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-08-31 20:29:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (28.08%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,19 +1,26 @@
 class Solution {
     public int specialArray(int[] nums) {
-       int n = nums.length;
+        int left=0;
+        int right=nums.length;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int count =countGreaterOrEqual(nums, mid); if (count > mid) {
+                left = mid + 1;
+            } else if (count < mid) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
 
-        for (int x = 0; x <= n; x++) {
-            int count = 0;
-            for (int num : nums) {
-                if (num >= x) {
-                    count++;
-                }
-            }
-            if (count == x) {
-                return x;
-            }
         }
-
         return -1;
+    }
+    public int countGreaterOrEqual(int[] nums, int x) {
+        int count = 0;
+        for (int num : nums)
+            if (num >= x) {
+                count++;
+            }
+        return count;
     }
 }
