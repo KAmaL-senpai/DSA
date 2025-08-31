0555a4d785380031a97fdfe403240d17f900f742
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-08-31 23:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 50.9 MB (7.31%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,11 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-      for (int i = 0; i < nums.length - 1; i += 2) {
-            if (nums[i] != nums[i + 1]) {
-                return nums[i];
-            }
+      int start=0;
+      int end=nums.length-1;
+      while(start<end){
+        int mid=start+(end-start)/2;
+        if(mid%2==1){
+            mid--;  //check for even coz the numbers are in pairs we want the first number 
         }
-        // If all previous elements were in pairs, the last one is unique
-        return nums[nums.length - 1];
+        if(nums[mid]==nums[mid+1]){
+            start=mid+2;
+        }else{
+            end=mid;
+        }
+      }
+      return nums[start];
+
     }
 }
