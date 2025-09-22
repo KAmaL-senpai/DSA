b1c57e4a674c0ee63fdc1b2f2d7e632a0a4b00d0
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-09-22 20:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (93.16%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,45 +1,39 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-         // Step 1: Frequency map of arr1
-        HashMap<Integer, Integer> map = new HashMap<>();
+         //find the max of the arr1
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr1.length; i++) {
-            int key = arr1[i];
-            if (map.containsKey(key)) {
-                map.put(key, map.get(key) + 1);
-            } else {
-                map.put(key, 1);
-            }
+            if (max < arr1[i])
+                max = arr1[i];
+        }
+        
+        //count the frequency of the arr2[i] in arr1
+        int[] countArr = new int[max + 1];
+        for (int i = 0; i < arr1.length; i++) {
+            int ele = arr1[i];
+            countArr[ele]++;
         }
 
-        List<Integer> ansList = new ArrayList<>();
-
-        // Step 2: Add elements in arr2 order
+        //add the ele according to frequency
+        int[] res = new int[arr1.length];
+        int j = 0;
         for (int i = 0; i < arr2.length; i++) {
-            if (map.containsKey(arr2[i])) {
-                for (int j = 0; j < map.get(arr2[i]); j++) {
-                    ansList.add(arr2[i]);
-                }
-                map.remove(arr2[i]); // Remove used keys
+            int ele = arr2[i];
+            while (countArr[ele] > 0) {
+                res[j] = ele;
+                j++;
+                countArr[ele]--;
             }
         }
-
-        // Step 3: Add remaining elements (not in arr2), sorted
-        List<Integer> remainingKeys = new ArrayList<>(map.keySet());
-        Collections.sort(remainingKeys);
-
-        for (int key : remainingKeys) {
-            int count = map.get(key);
-            for (int j = 0; j < count; j++) {
-                ansList.add(key);
+        
+        //fill the remaining sorted order
+        for (int i = 0; i < countArr.length; i++) {
+            while (countArr[i] > 0) {
+                res[j] = i;
+                j++;
+                countArr[i]--;
             }
         }
-
-        // Step 4: Convert List to int[]
-        int[] ans = new int[ansList.size()];
-        for (int i = 0; i < ansList.size(); i++) {
-            ans[i] = ansList.get(i);
-        }
-
-        return ans;
+        return res;
     }
 }
