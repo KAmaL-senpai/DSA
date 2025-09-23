0290efe099ada171be644221bd6bc5fca95aa2fa
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-09-23 23:38:17 +0530

    Time: 12 ms (98.14%) | Memory: 43.2 MB (77.76%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,26 +1,29 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+         Arrays.sort(nums);
         int min = Integer.MAX_VALUE;
-        int closestSum = 0;
-
+        int closeSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    int possibleAns = Math.abs(sum - target);
-
-                    if (possibleAns == 0) {
-                        return sum; // Perfect match found
-                    }
-
-                    if (possibleAns < min) {
-                        min = possibleAns;
-                        closestSum = sum;
-                    }
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                int possibleAns = Math.abs(sum - target);
+                if (possibleAns == 0)
+                    return sum;
+                if (possibleAns < min) {
+                    min = possibleAns;
+                    closeSum = sum;
+                }
+                if (sum < target) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-
-        return closestSum;
+        return closeSum;
     }
 }
