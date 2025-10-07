a05ec8ac4af4d7049e452f7db46a2d255513c7f3
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-10-07 19:36:37 +0530

    Time: 1 ms (39.21%) | Memory: 41.7 MB (30.31%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -6,8 +6,7 @@ class Solution {
         Arrays.sort(strArr, (a, b) -> a.charAt(a.length() - 1) - b.charAt(b.length() - 1));
         
         for (String str : strArr) {
-            String stri=str.substring(0, str.length() - 1);
-            ans.append(stri+" ");
+                ans.append(str.substring(0, str.length() - 1)).append(" ");
         }
 
         return ans.toString().trim();
