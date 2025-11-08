13c921103c45a3344751505f8a5de1566162ff79
Kamal sing bhumij <113301075+KAmaL-senpai@users.noreply.github.com>
2025-11-08 17:46:04 +0530

    Time: 3 ms (51.83%) | Memory: 43.5 MB (14.98%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+            if (num1.equals("0") || num2.equals("0"))
+            return "0";
+
+        // intialize arr to hold the result of multiplication
+        int[] resultArr = new int[num1.length() + num2.length()];
+
+        // Reversing the both the string
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+
+                // multiplication
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+                int mul = digit1 * digit2;
+
+                // find positions in the result array where to add the result;
+                int posLast = i + j + 1;
+                int posFirst = i + j;
+
+                // add the multiplication result to the current position and handle carry
+                int sum = mul + resultArr[posLast];
+
+                resultArr[posLast] = sum % 10; // set the current position to the remainder;
+                resultArr[posFirst] += sum / 10; // add the carry to the next higher position
+
+            }
+        }
+
+        //Conversion to string for the resultArr;
+
+        StringBuilder ans = new StringBuilder();
+        for (int num : resultArr) {
+            //skip leading zeroes
+            if (!(ans.length() == 0 && num == 0)) {
+                ans.append(num);
+            }
+        }
+
+        return new String(ans);
+    }
+}
